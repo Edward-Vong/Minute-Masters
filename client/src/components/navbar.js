e875385b27d5fcf31dcc9d2ea12cf075613d21fd
PandaIsAwkward <evusa142@gmail.com>
2023-12-05 23:56:54 -0800

    completed new navBar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,47 +1,36 @@
 import React from "react";
- 
-// We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
-import { Link } from "react-router-dom";
- 
+import calendar from "./icons/calendar.png";
+import { Link } from 'react-router-dom';
+
 
 // Here, we display our Navbar
 export default function Navbar() {
- return (
-  <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  return (
+    <div class="container">
+      <nav class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="col-md-3 mb-2 mb-md-0 link-body-emphasis text-decoration-none">
+          <img src={ calendar } class="bi me-2" width="40" height="40"/>
+          <span class="fs-4">Minute Masters</span>
+        </a>
 
-    <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-      <a class="navbar-brand col-lg-3 me-0" href="#">Minute Masters</a>
-      <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" to="/home" >Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <div class="d-lg-flex col-lg-3 justify-content-lg-end">
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2">Features</a></li>
+          <li><a href="#" class="nav-link px-2">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2">About</a></li>
+        </ul>
 
-        <button class="btn btn-primary">
-          <Link className="nav-link" to="/login">Login</Link>
-        </button>
-        <span style={{ marginRight: '10px' }}></span>
-        <button class="btn btn-primary">
-          <Link className="nav-link" to="/register">Register</Link>
-        </button>
-
-      </div>
+        <div class="col-md-3 text-end">
+          <Link to="/login">
+            <button　type="button" class="btn btn-primary me-2">Login</button>
+          </Link>
+          <Link to="/register">
+            <button　type="button" class="btn btn-primary me-2">register</button>
+          </Link>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
- );
+  );
 }
