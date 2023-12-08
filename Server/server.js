90833be5c7e3fadbbaef455172dd0ead42d484b2
Dean <tepigmastertrainer@gmail.com>
2023-12-07 20:26:57 -0800

    git push

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
