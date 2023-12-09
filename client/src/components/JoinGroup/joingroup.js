1b434f56bf42ced8cdab18f3478a135af8c51be3
Dean <tepigmastertrainer@gmail.com>
2023-12-08 18:46:05 -0800

    join group test

--- client/src/components/JoinGroup/joingroup.js
+++ client/src/components/JoinGroup/joingroup.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 const JoinGroup = () => {
-    
+  const [code, setCode] = useState("");
+  const [validCode, setValidCode] = useState(false);
+  const [message, setMessage] = useState("");
+  
+  const token = localStorage.getItem('token');
+  const isLoggedIn = !!token;
+
+  const checkCodeValidity = () => {
+    if (isLoggedIn) {
+      // If logged in, check the code validity
+      if (code === "YOUR_VALID_CODE") {
+        setValidCode(true);
+        setMessage("Code is valid. You have joined the group!");
+      } else {
+        setValidCode(false);
+        setMessage("Invalid code. Unable to join the group.");
+      }
+    } else {
+      // If not logged in, display an appropriate message
+      setMessage("Not logged in. Unable to join the group.");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setCode(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h2>Join Group</h2>
+      <div className="form-group">
+        <label>Enter Group Code:</label>
+        <input
+          type="text"
+          className="form-control"
+          value={code}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button className="btn btn-primary" onClick={checkCodeValidity}>
+        Join Group
+      </button>
+      {message && <p>{message}</p>}
+    </div>
+  );
 };
 
 export default JoinGroup;
