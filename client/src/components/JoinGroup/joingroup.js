0f717f70acf7a25a8dacb87600762e1ac8b03a29
Dean <tepigmastertrainer@gmail.com>
2023-12-08 19:39:22 -0800

    real this time

--- client/src/components/JoinGroup/joingroup.js
+++ client/src/components/JoinGroup/joingroup.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Home from "./Home";
+
 
 const JoinGroup = ({ setJoinedGroup }) => {
   const [code, setCode] = useState("");
